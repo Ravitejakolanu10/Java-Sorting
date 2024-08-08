ac0eb77876d145955a39d2c6bd77eb63da1d0b2d
ravi teja <ravitejakolanu10@gmail.com>
2024-08-08 23:26:50 +0530

    Selection Sort

--- Selection_Sort.java
+++ Selection_Sort.java
@@ -1,3 +1,23 @@
 public class Selection_Sort {
-    
+    public static void main(String[] args) {
+        int [] a={5,3,1,4,2};
+        for(int i =0 ;i<a.length-1;i++)
+        {
+            for(int j=i+1;j<a.length;j++)
+            {
+                int smallest=i;
+                if(a[i]>a[j])
+                {
+                    smallest=j;
+                }
+                int temp=a[smallest];
+                    a[smallest]=a[i];   
+                    a[i]=temp; 
+            }    
+        }
+        for(int i =0 ;i<a.length;i++)
+        {
+            System.out.print(a[i]+ "|");
+        }
+    }
 }
